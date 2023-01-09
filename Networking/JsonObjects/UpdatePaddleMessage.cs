0d3981e75687cfe5d6cafde65c76e2da3830e5a6
Marcoot <marco.bosker@hva.nl>
2023-01-09 10:04:17 +0100

    Add project files.

---
+++ Networking/JsonObjects/UpdatePaddleMessage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Networking.JsonObjects
+{
+    public class UpdatePaddleMessage : MessagePacket
+    {
+        public Vector2 position;
+        public UpdatePaddleMessage() 
+        {
+            msgType = "UPDATE_POS";        
+        }
+    }
+}
