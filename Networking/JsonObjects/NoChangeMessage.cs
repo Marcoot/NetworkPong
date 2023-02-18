ecfabfdeb8adc40204828e46e38b3063cec63193
Marcoot <marco.bosker@hva.nl>
2023-02-18 19:39:02 +0100

    stap 4

---
+++ Networking/JsonObjects/NoChangeMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Networking.JsonObjects
+{
+    public class NoChangeMessage : MessagePacket
+    {
+        public int tickNumber;
+        public string direction {  get; set; }
+
+        public NoChangeMessage()
+        {
+
+        }
+    }
+}
